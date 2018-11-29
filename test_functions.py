347587aa8dfa319f3fc9d27f6ee017477fabc973
AlexSlav <alexander.slavutin@rambler.ru>
2018-11-29 20:56:55 +0300

    More tests

--- test_functions.py
+++ test_functions.py
@@ -18,27 +18,26 @@ def test_date_check():
 
 
 def test_name_check():
-    assert func.name_check('Sasha Sllavutin') == 'Sasha Sllavutin'
-    assert func.name_check('Sasha sllavutin') == 'Sasha Sllavutin'
-    assert func.name_check('sasha sllavutin') == 'Sasha Sllavutin'
+    assert func.name_check('Sasha Sllavutin') == 1
+    assert func.name_check('sasha sllavutin') == 1
     assert func.name_check('Petr') == 0
     assert func.name_check('Sasd.sdfsdf sdf.sdf.sdfsdf') == 0
     assert func.name_check('123324646463452 12341321234') == 0
     assert func.name_check('_____ asdfasdfsdf _____') == 0
     assert func.name_check('____ _____') == 0
-    assert func.name_check('Sasha1 Petrov') == 'Sasha1 Petrov'
+    assert func.name_check('Sasha1 Petrov') == 1
 
 
 def test_number_check():
-    assert func.number_check('89101418456') == '89101418456'
+    assert func.number_check('89101418456') == 1
     assert func.number_check('8910141845') == 0
-    assert func.number_check('+79101418456') == '89101418456'
+    assert func.number_check('+79101418456') ==1
     assert func.number_check('+79101418456123123') == 0
     assert func.number_check('Wrong input') == 0
     assert func.number_check('89101sdffsdf') == 0
     assert func.number_check('+7+7+7+7+7+7+7+7+7+7+7') == 0
     assert func.number_check('8910 123123 123123') == 0
-    assert func.number_check('+79101412356') == '89101412356'
+    assert func.number_check('+79101412356') == 1
     assert func.number_check('+7910141845+7') == 0
 
 
